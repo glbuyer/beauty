b21dfe4dd891e993004bf5a79f4d3ac4a2df4828
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-27 22:34:49 +1100

    x

--- scripts/main.php
+++ scripts/main.php
@@ -1,9 +1,9 @@
 <?php
-$matchBinary = '/Users/xiaojiew1/Applications/anaconda3/envs/py36/bin/match';
-$imgFilepath = '/Users/xiaojiew1/Projects/beauty/data/ye.png';
+$matchBinary = '/root/anaconda3/envs/py36/bin/match';
+$imgFilepath = '../data/ye.png';
 
 
-$fullCommand = sprintf('%s %s', $matchBinary, $imgFilepath);
+$fullCommand = sprintf('%s --image_file %s', $matchBinary, $imgFilepath);
 // echo $fullCommand;
 $matchResult = shell_exec($fullCommand);
 $matchResult = json_decode($matchResult, true);
