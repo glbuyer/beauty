1f616beabab2bedde229b20cfd5193e8cf49442e
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-27 22:58:41 +1100

    x

--- scripts/main.php
+++ scripts/main.php
@@ -1,22 +1,16 @@
 <?php
 $matchBinary = '/root/anaconda3/envs/py36/bin/match';
 $imgFilepath = '../data/xiaojie.jpg';
-
-
+$imgFilepath = '/var/www/glbuyer_face_features_api_server_test/storage/app/images/customerUpload/5aba318a36594.jpg'
 $fullCommand = sprintf('%s --image_file %s', $matchBinary, $imgFilepath);
-// echo $fullCommand;
 $matchResult = shell_exec($fullCommand);
 $matchResult = json_decode($matchResult, true);
 var_dump($matchResult);
 
 $matchBinary = '/root/anaconda3/envs/py36/bin/match';
 $imageUrl = 'https://asset-face-features.glbuyer.com/72c7afac29c1b102b7567231f1a24b20.jpg';
-
-
 $fullCommand = sprintf('%s --image_url %s', $matchBinary, $imageUrl);
-// echo $fullCommand;
 $matchResult = shell_exec($fullCommand);
 $matchResult = json_decode($matchResult, true);
 var_dump($matchResult);
-
 ?>
