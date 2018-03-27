e097a499c49aa31839b76a81ea44ce7a8ca9797c
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-03-27 22:37:00 +1100

    x

--- scripts/main.php
+++ scripts/main.php
@@ -8,4 +8,15 @@ $fullCommand = sprintf('%s --image_file %s', $matchBinary, $imgFilepath);
 $matchResult = shell_exec($fullCommand);
 $matchResult = json_decode($matchResult, true);
 var_dump($matchResult);
+
+$matchBinary = '/root/anaconda3/envs/py36/bin/match';
+$imageUrl = 'https://asset-face-features.glbuyer.com/72c7afac29c1b102b7567231f1a24b20.jpg';
+
+
+$fullCommand = sprintf('%s --image_url %s', $matchBinary, $imageUrl);
+// echo $fullCommand;
+$matchResult = shell_exec($fullCommand);
+$matchResult = json_decode($matchResult, true);
+var_dump($matchResult);
+
 ?>
