cbeb129426bfa7df17d1bc62fa2122f0191b13dc
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-04-14 22:48:40 +1000

    x

---
+++ scripts/demo.sh
@@ -0,0 +1,16 @@
+match --image_file data/beauty_0.jpg
+# match --image_file data/beauty_1.jpg
+# match --image_file data/beauty_2.jpeg
+# match --image_file data/beauty_3.jpeg
+# match --image_file data/beauty_4.jpeg
+
+match --image_file data/normal_0.jpg
+# match --image_file data/normal_1.jpeg
+# match --image_file data/normal_2.jpeg
+# match --image_file data/normal_3.jpeg
+# match --image_file data/normal_4.jpeg
+
+match --image_file data/ugly_0.jpg
+# match --image_file data/ugly_1.jpeg
+# match --image_file data/ugly_2.jpeg
+# match --image_file data/ugly_3.jpeg
