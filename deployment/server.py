13ca3635ae048b2f86b5bdfd94a1bb8f60138f72
xiaojiew1 <xiaojiew1@student.unimelb.edu.au>
2018-10-07 13:59:09 +1100

    x

--- deployment/server.py
+++ deployment/server.py
@@ -32,4 +32,4 @@ def index():
 # python server.py
 if __name__ == '__main__':
     # server.run(host='0.0.0.0', port=3000, threaded=True)
-    server.run(debug=True, port=3000, threaded=True)
+    server.run(debug=True, port=5000, threaded=True)
